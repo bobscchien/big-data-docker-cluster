33d90addecdb759402ba41cf2c8bc3c5519283c9
bobchien <bob.sc.chien@gmail.com>
2022-02-02 00:04:24 +0800

    change the restart policy

--- bin/yml-builder.sh
+++ bin/yml-builder.sh
@@ -29,12 +29,13 @@ cat <<EOF >> $file
     user: hadoop
     volumes:
       - .:$PROJECT_CODE:ro
-      - ${DIR_DATA_TOP}1/$DIR_DATA_APP/$NAMENODE_NAME:$PROJECT_DATA/hadoop/tmp/name:rw
+      - ${DIR_DATA_TOP}1/$DIR_DATA_APP/${NAMENODE_NAME}1:$PROJECT_DATA/hadoop:rw
     networks:
       custom-net:
         ipv4_address: ${NAMENODE_NET}.1
     ports:
       - 18020:8020
+      - 18088:8088
       - 19870:9870
       - 19888:19888
     deploy:
@@ -42,10 +43,9 @@ cat <<EOF >> $file
         limits:
           cpus: $NAMENODE_CPU
           memory: $NAMENODE_MEM
-    restart: always
-    command: $PROJECT_CODE/activate-hadoop.sh nna $1
+    restart: unless-stopped
+    command: $PROJECT_CODE/bin_container/initialize-hadoop.sh $1
     depends_on:
-      - ${NAMENODE_NAME}2
 EOF
 
 for n in $(seq 1 $ZOOKEEPER_NUM);
@@ -62,13 +62,6 @@ cat <<EOF >> $file
 EOF
 done;
 
-for n in $(seq 1 $YARN_NUM);
-do 
-cat <<EOF >> $file
-      - ${YARN_NAME}$n
-EOF
-done;
-
 for n in $(seq 2 $NAMENODE_NUM);
 do
 cat <<EOF >> $file
@@ -79,12 +72,13 @@ cat <<EOF >> $file
     user: hadoop
     volumes:
       - .:$PROJECT_CODE:ro
-      - ${DIR_DATA_TOP}${n}/$DIR_DATA_APP/$NAMENODE_NAME:$PROJECT_DATA/hadoop/tmp/name:rw
+      - ${DIR_DATA_TOP}${n}/$DIR_DATA_APP/${NAMENODE_NAME}2:$PROJECT_DATA/hadoop:rw
     networks: 
       custom-net:
         ipv4_address: ${NAMENODE_NET}.${n}
     ports:
       - 28020:8020
+      - 28088:8088
       - 29870:9870
       - 29888:19888
     deploy:
@@ -92,41 +86,10 @@ cat <<EOF >> $file
         limits:
           cpus: $NAMENODE_CPU
           memory: $NAMENODE_MEM
-    restart: always
-    command: $PROJECT_CODE/activate-hadoop.sh nns
-EOF
-done;
-
-# ===================== Hadoop - Yarn ===================== #
-
-cat <<EOF >> $file
-
-### Hadoop - Yarn / Resource Manager
-
-EOF
-
-for n in $(seq 1 $YARN_NUM);
-do 
-cat <<EOF >> $file
-  ${YARN_NAME}${n}:
-    container_name: ${YARN_NAME}${n}
-    hostname: ${YARN_NAME}${n}
-    image: $USER_DOCKER/apache-hadoop-spark:$HADOOP_VER-$SPARK_VER
-    user: hadoop
-    volumes:
-      - .:$PROJECT_CODE:ro
-    networks:
-      custom-net:
-        ipv4_address: ${YARN_NET}.${n}
-    ports:
-      - ${n}8088:8088
-    deploy:
-      resources:
-        limits:
-          cpus: $YARN_CPU
-          memory: $YARN_MEM
-    restart: always
-    command: $PROJECT_CODE/activate-hadoop.sh rm${n}
+    restart: unless-stopped
+    command: $PROJECT_CODE/bin_container/initialize-hadoop.sh $1
+    depends_on:
+      - ${NAMENODE_NAME}1
 EOF
 done;
 
@@ -148,7 +111,7 @@ cat <<EOF >> $file
     user: hadoop
     volumes:
       - .:$PROJECT_CODE:ro
-      - ${DIR_DATA_TOP}${n}/$DIR_DATA_APP/$DATANODE_NAME:$PROJECT_DATA/hadoop/tmp/data:rw
+      - ${DIR_DATA_TOP}${n}/$DIR_DATA_APP/${DATANODE_NAME}${n}:$PROJECT_DATA/hadoop:rw
     networks:
       custom-net:
         ipv4_address: ${DATANODE_NET}.${n}
@@ -157,8 +120,8 @@ cat <<EOF >> $file
         limits:
           cpus: $DATANODE_CPU
           memory: $DATANODE_MEM
-    restart: always
-    command: $PROJECT_CODE/activate-hadoop.sh djn
+    restart: unless-stopped
+    command: $PROJECT_CODE/bin_container/initialize-hadoop.sh $1
 EOF
 done;
 
@@ -180,7 +143,7 @@ cat <<EOF >> $file
     user: zookeeper
     volumes:
       - .:$PROJECT_CODE:ro
-      - ${DIR_DATA_TOP}${n}/$DIR_DATA_APP/${ZOOKEEPER_NAME}:$PROJECT_DATA/zookeeper:rw
+      - ${DIR_DATA_TOP}${n}/$DIR_DATA_APP/${ZOOKEEPER_NAME}${n}:$PROJECT_DATA/zookeeper:rw
     networks:
       custom-net:
         ipv4_address: ${ZOOKEEPER_NET}.${n}
@@ -192,8 +155,8 @@ cat <<EOF >> $file
         limits:
           cpus: $ZOOKEEPER_CPU
           memory: $ZOOKEEPER_MEM
-    restart: always
-    command: $PROJECT_CODE/activate-zookeeper.sh --id ${n}
+    restart: unless-stopped
+    command: $PROJECT_CODE/bin_container/initialize-zookeeper.sh --id ${n}
 EOF
 done;
 
@@ -215,7 +178,7 @@ cat <<EOF >> $file
     user: kafka
     volumes:
       - .:$PROJECT_CODE:ro
-      - ${DIR_DATA_TOP}${n}/$DIR_DATA_APP/${KAFKA_NAME}:$PROJECT_DATA/kafka:rw
+      - ${DIR_DATA_TOP}${n}/$DIR_DATA_APP/${KAFKA_NAME}${n}:$PROJECT_DATA/kafka:rw
     networks:
       custom-net:
         ipv4_address: ${KAFKA_NET}.${n}
@@ -226,8 +189,8 @@ cat <<EOF >> $file
         limits:
           cpus: $KAFKA_CPU
           memory: $KAFKA_MEM
-    restart: always
-    command: $PROJECT_CODE/activate-kafka.sh --id ${n}
+    restart: unless-stopped
+    command: $PROJECT_CODE/bin_container/initialize-kafka.sh --id ${n}
     depends_on:
 EOF
 
